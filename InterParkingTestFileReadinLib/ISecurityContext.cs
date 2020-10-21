7c409d5e166a2e2da98fe4c0cee862fe94bad340
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-22 01:36:21 +0200

    implement RestrictedXmlFileReader

---
+++ InterParkingTestFileReadinLib/ISecurityContext.cs
@@ -0,0 +1,10 @@
+﻿namespace InterParkingTestFileReadinLib
+{
+    /// <summary>
+    /// Defines whether reading a file is allowed or not (and why), in a particular context
+    /// </summary>
+    public interface ISecurityContext
+    {
+        bool AllowsToReadFile(string path, out string reasonForDenial);
+    }
+}
