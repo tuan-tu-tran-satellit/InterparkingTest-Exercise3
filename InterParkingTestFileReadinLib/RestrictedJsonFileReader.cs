db364566f80832f1339d07670965f13f728da9b9
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-22 02:32:53 +0200

    implement security role based json reader

---
+++ InterParkingTestFileReadinLib/RestrictedJsonFileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using System.Text;
+
+namespace InterParkingTestFileReadinLib
+{
+    /// <summary>
+    /// A file reader that consults a security context before reading a file.
+    /// And then validates that the file content is indeed json.
+    /// </summary>
+    public class RestrictedJsonFileReader : RestrictedFileReader
+    {
+        public RestrictedJsonFileReader(ISecurityContext securityContext, IFileReader fileReader = null)
+            : base(securityContext, new JsonFileReader(fileReader))
+        {
+        }
+    }
+}
---
+++ Tests/RestrictedJsonFileReaderTest.cs
@@ -0,0 +1,78 @@
+﻿using FluentAssertions;
+using InterParkingTestFileReadinLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class RestrictedJsonFileReaderTest
+    {
+        [TestMethod]
+        public void ItCanEnforceASecurityPolicy()
+        {
+            //Arrange
+            var securityPolicy = new Mock<ISecurityContext>(MockBehavior.Strict);
+
+            var somePath = "/some/path.json";
+            var reasonForDenial = "this user cannot read anything";
+            securityPolicy.Setup(s => s.AllowsToReadFile(somePath, out reasonForDenial)).Returns(false);
+
+            var restrictedReader = new RestrictedJsonFileReader(securityPolicy.Object);
+
+            //Act
+            Action readingTheFile = () => restrictedReader.ReadFileContent(somePath);
+
+            //Assert
+            readingTheFile.Should().Throw<SecurityException>()
+                .Which.Message.Should().Contain(somePath).And.Contain(reasonForDenial);
+        }
+
+        [TestMethod]
+        public void It_Can_Read_JsonFiles_If_The_SecurityContext_Allows_It()
+        {
+            //Arrange
+            var securityPolicy = new Mock<ISecurityContext>(MockBehavior.Strict);
+
+            var somePath = "/some/path.json";
+            string reasonForDenial = null;
+            securityPolicy.Setup(s => s.AllowsToReadFile(somePath, out reasonForDenial)).Returns(true);
+
+            var fakeReader = new FakeReader { Content = "{'some':['json','content']}" };
+
+            var restrictedReader = new RestrictedJsonFileReader(securityPolicy.Object, fakeReader);
+
+            //Act
+            var result = restrictedReader.ReadFileContent(somePath);
+
+            //Assert
+            result.Should().Be(fakeReader.Content);
+        }
+
+        [TestMethod]
+        public void It_Validates_The_JsonContent_When_Allowed_By_The_SecurityContext()
+        {
+            //Arrange
+            var securityPolicy = new Mock<ISecurityContext>(MockBehavior.Strict);
+
+            var somePath = "/some/path.txt";
+            string reasonForDenial = null;
+            securityPolicy.Setup(s => s.AllowsToReadFile(somePath, out reasonForDenial)).Returns(true);
+
+            var fakeReader = new FakeReader { Content = "not json content" };
+
+            var restrictedReader = new RestrictedJsonFileReader(securityPolicy.Object, fakeReader);
+
+            //Act
+            Action readingTheFile = () => restrictedReader.ReadFileContent(somePath);
+
+            //Assert
+            readingTheFile.Should().Throw<InvalidDataException>();
+        }
+    }
+}
