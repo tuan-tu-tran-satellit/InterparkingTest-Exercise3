2b9c998341e5b42249d2847db8e1e7a2de7775b5
Tuan-Tu Tran <tuan-tu.tran@satellit.be>
2020-10-22 02:28:44 +0200

    implement encypted json reader

---
+++ InterParkingTestFileReadinLib/EncryptedJsonFileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterParkingTestFileReadinLib
+{
+    public class EncryptedJsonFileReader : IFileReader
+    {
+        private readonly JsonFileReader _reader;
+
+        public EncryptedJsonFileReader(Func<string, string> algorithm = null, IFileReader fileReader = null)
+        {
+            _reader = new JsonFileReader(new EncryptedFileReader(algorithm, fileReader));
+        }
+
+        public string ReadFileContent(string path)
+        {
+            return _reader.ReadFileContent(path);
+        }
+    }
+}
---
+++ Tests/EncryptedJsonFileReaderTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using InterParkingTestFileReadinLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass]
+    public class EncryptedJsonReaderTest
+    {
+        [TestMethod]
+        public void ItCanDecryptAndValidateJsonContent()
+        {
+            //Arrange
+            var fakeReader = new FakeReader { Content = "}llun:'oof'{" };
+            var encryptedJsonReader = new EncryptedJsonFileReader(fileReader: fakeReader);
+
+            //Act
+            var result = encryptedJsonReader.ReadFileContent("some/path.json.encrypted");
+
+            //Assert
+            result.Should().Be("{'foo':null}");
+        }
+
+        [TestMethod]
+        public void ItRejectsNonJsonContent()
+        {
+            //Arrange
+            var fakeReader = new FakeReader { Content = "some encrypted but not json content" };
+            var encryptedJsonReader = new EncryptedJsonFileReader(fileReader: fakeReader);
+
+            //Act
+            Action readingTheFile = () => encryptedJsonReader.ReadFileContent("some/path");
+
+            //Assert
+            readingTheFile.Should().Throw<InvalidDataException>();
+        }
+    }
+}
